bae7745785242e225cecb2f83576be59984f0c2f
Eder Gutierrez <gutierrezeder64@gmail.com>
2026-01-31 15:54:16 -0600

    Remove vary and wrappy modules from node_modules
    - Deleted the vary module, including its index.js and package.json files.
    - Removed the wrappy module, including its LICENSE, README.md, package.json, and wrappy.js files.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 
+import {handlerReadiness} from "./api/readiness.js";
+
 const app = express();
 const PORT = 8080;
 
-app.use(express.static(".")); //relative to the process cwd
+app.use("/app", express.static("./src/app")); //relative to the process cwd
+
+
+app.get("/healthz", handlerReadiness)
 
 app.listen(PORT, () => { //Starts server and listen for connections on the port
     console.log(`Server is running at http://localhost${PORT}`);
